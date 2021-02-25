5dd48c360383bfc779a3ddfcd98a9fd2a9e65b6b
HL <>
2021-02-25 22:46:16 +0800

    update

--- utils/address.js
+++ utils/address.js
@@ -1,4 +1,4 @@
-const isSameAddress = (address1, address2) = address1.toLowerCase() === address2.toLowerCase();
+const isSameAddress = (address1, address2) => address1.toLowerCase() === address2.toLowerCase();
 
 module.exports = {
   isSameAddress,
