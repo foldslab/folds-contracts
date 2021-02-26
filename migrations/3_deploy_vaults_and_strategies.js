22fd9e1247fb25399e55c447c4e1f4db817fda5d
HL <>
2021-02-27 01:47:42 +0800

    fix import issue

--- migrations/3_deploy_vaults_and_strategies.js
+++ migrations/3_deploy_vaults_and_strategies.js
@@ -17,7 +17,7 @@ const hecoAddresses = require('../constants/hecoAddresses')
 const deployedContracts = require('../constants/deployedContracts')
 console.log('deployedContracts: ', deployedContracts);
 
-import utils from '../utils/address'
+const utils = require('../utils/address')
 
 const vaultsToBeDeployed = [
     'HBTC_USDT',
