c21aa0aa4b18e771d22d4b4196123c29638bbcbe
HL <>
2021-02-27 21:55:59 +0800

    refactor: use checksum address

--- constants/fildaAddresses.js
+++ constants/fildaAddresses.js
@@ -1,14 +1,14 @@
 module.exports = {
   common: {
     comptroller: '0xb74633f2022452f377403B638167b0A135DB096d',
-    comp: '0xe36ffd17b2661eb57144ceaef942d95295e637f0',
+    comp: '0xE36FFD17B2661EB57144cEaEf942D95295E637F0',
   },
   HUSD: {
-    underlying:	'0x0298c2b32eae4da002a15f36fdf7615bea3da047',
+    underlying:	'0x0298c2b32eaE4da002a15f36fdf7615BEa3DA047',
     ctoken:	'0xB16Df14C53C4bcfF220F4314ebCe70183dD804c0',
   },
   USDT: {
-    underlying:	'0xa71edc38d189767582c38a3145b5873052c3e47a',
+    underlying:	'0xa71EdC38d189767582C38A3145b5873052c3e47a',
     ctoken:	'0xAab0C9561D5703e84867670Ac78f6b5b4b40A7c1',
   }
 };
